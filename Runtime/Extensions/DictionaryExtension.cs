3ecb53c9c117494e11d7fe80a971727188d6aa1c
Yurii Lunha <yurii.lunha.dev@gmail.com>
2023-10-28 19:55:23 -0600

    feature: name convention builder & dictionary extension

---
+++ Runtime/Extensions/DictionaryExtension.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Lunha.DevKit.Utilities;
+
+namespace Lunha.DevKit.Extensions
+{
+    public static class DictionaryExtension
+    {
+        /// <summary>
+        /// Returns the composition of the structure's public fields
+        /// </summary>
+        /// <param name="data">Structure's instance</param>
+        /// <param name="nameConvention">Dictionary keys name convention</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>Dictionary of key(string) / value(object)</returns>
+        public static Dictionary<string, object> ToDictionary<T>(this T data, NameConventionType nameConvention)
+        {
+            var dictionary = new Dictionary<string, object>();
+            var fields = data.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var fieldInfo in fields)
+            {
+                var key = NameConventionBuilder.FormatTextByConvention(fieldInfo.Name, nameConvention);
+                dictionary.Add(key, fieldInfo.GetValue(data));
+            }
+
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Returns an instance of T with filled public fields by dictionary
+        /// </summary>
+        /// <param name="dictionary">Dictionary of fields</param>
+        /// <param name="nameConvention">T fields name convention</param>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <returns>Instance of T or null</returns>
+        public static T ToObject<T>(this Dictionary<string, object> dictionary, NameConventionType nameConvention)
+            where T : class
+        {
+            if (Activator.CreateInstance(typeof(T)) is not T objectInstance)
+            {
+                return null;
+            }
+
+            var fields = objectInstance.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var keyValuePair in dictionary)
+            {
+                var fieldName = NameConventionBuilder.FormatTextByConvention(keyValuePair.Key, nameConvention);
+                var fieldInfo = fields.FirstOrDefault(i => i.Name.Equals(fieldName));
+
+                if (fieldInfo == null)
+                {
+#if UNITY_EDITOR || ENVIRONMENT_DEV
+                    UnityEngine.Debug.LogWarning(
+                        $"Casting Dictionary to {nameof(T)}: can't find field with field name '{fieldName}'");
+#endif
+                    continue;
+                }
+
+                if (fieldInfo.FieldType.IsArray)
+                {
+                    var toObjectMethod = typeof(DictionaryExtension).GetMethod(nameof(ToObject));
+                    if (toObjectMethod == null)
+                    {
+                        throw new ReflectionTypeLoadException(
+                            new[] { typeof(DictionaryExtension) },
+                            new Exception[] { new MissingMethodException() },
+                            $"Method '{nameof(ToObject)}' not found in '{typeof(DictionaryExtension).Assembly.FullName}'");
+                    }
+
+                    var fieldArrayElementType = fieldInfo.FieldType.GetElementType();
+                    if (fieldArrayElementType == null)
+                    {
+                        throw new ReflectionTypeLoadException(
+                            new[] { typeof(T) },
+                            new Exception[] { new MissingFieldException() },
+                            $"Field of array '{fieldInfo.FieldType.Name}' is null in '{fieldInfo.GetType().FullName}'");
+                    }
+
+                    var toObjectMethodInfo = toObjectMethod.MakeGenericMethod(fieldArrayElementType);
+                    var inputValues = (List<object>)keyValuePair.Value;
+                    var valueInstancesArray = Array.CreateInstance(fieldArrayElementType, inputValues.Count);
+
+                    for (var index = 0; index < inputValues.Count; index++)
+                    {
+                        var valueElementInstance =
+                            toObjectMethodInfo.Invoke(null, new[] { inputValues[index], nameConvention });
+                        valueInstancesArray.SetValue(valueElementInstance, index);
+                    }
+
+                    fieldInfo.SetValue(objectInstance, valueInstancesArray);
+                }
+                else
+                {
+                    fieldInfo.SetValue(objectInstance, Convert.ChangeType(keyValuePair.Value, fieldInfo.FieldType));
+                }
+            }
+
+            return objectInstance;
+        }
+    }
+}
---
+++ Runtime/Utilities/NameConventionBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Lunha.DevKit.Utilities
+{
+    public static class NameConventionBuilder
+    {
+        public static string FormatTextByConvention(string text, NameConventionType convention)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return string.Empty;
+            }
+
+            text = text.Replace('-', ' ');
+            text = text.Replace('_', ' ');
+
+            RemoveChar(ref text, '\'');
+            RemoveChar(ref text, '\"');
+
+            RemoveDoubleSpaces(ref text);
+
+            text = text.Trim();
+            text = text.Trim('.');
+
+            switch (convention)
+            {
+                case NameConventionType.CamelCase:
+                {
+                    var output = Regex
+                        .Replace(text, @"\b(\w)", match => match.Value.ToUpper())
+                        .Replace(" ", "");
+
+                    return char.ToLower(output[0]) + output.Substring(1);
+                }
+                case NameConventionType.SnakeCase:
+                {
+                    var output = Regex.Replace(text, @"(\p{Ll})(\p{Lu})|(\W)+", "$1_$2");
+                    return output.ToLower();
+                }
+                case NameConventionType.KebabkCase:
+                {
+                    return Regex.Replace(text, @"(\p{Ll})(\p{Lu})|(\W)+", "$1-$2").ToLower();
+                }
+                case NameConventionType.PascalCase:
+                {
+                    var output = Regex.Replace(text, @"(?:^|\s)(\w)", match => match.Value.ToUpper());
+                    return output.Replace(" ", "");
+                }
+                case NameConventionType.None:
+                default:
+                {
+                    return text;
+                }
+            }
+        }
+
+        private static void RemoveChar(ref string text, char c)
+        {
+            for (var i = text.Count(e => e.Equals(c)); i > 0; i--)
+            {
+                text = text.Remove(text.IndexOf(c), 1);
+            }
+        }
+
+        private static void RemoveDoubleSpaces(ref string text)
+        {
+            text = text.Trim();
+
+            const string doubleSpace = "  ";
+            while (text.Contains(doubleSpace))
+            {
+                var index = text.IndexOf(doubleSpace, StringComparison.Ordinal);
+                text = text.Remove(index, 1);
+            }
+        }
+    }
+
+    public enum NameConventionType
+    {
+        /// <summary>
+        /// No formatting would apply to provided user variant.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Example: firstName
+        /// </summary>
+        CamelCase = 1,
+
+        /// <summary>
+        /// Example: first_name
+        /// </summary>
+        SnakeCase = 2,
+
+        /// <summary>
+        /// Example: first-name 
+        /// </summary>
+        KebabkCase = 3,
+
+        /// <summary>
+        /// Example: FirstName
+        /// </summary>
+        PascalCase = 4,
+    }
+}
