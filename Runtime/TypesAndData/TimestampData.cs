178d597c83684af74d99946bd5d75c92c8f0b832
Yurii Lunha <yurii.lunha.dev@gmail.com>
2023-12-02 22:37:37 -0700

    feat: types & data

---
+++ Runtime/TypesAndData/TimestampData.cs
@@ -0,0 +1,31 @@
+﻿using static System.DateTime;
+
+namespace Lunha.DevKit.TypesAndData
+{
+    [System.Serializable]
+    public class TimestampData
+    {
+        const string DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+        string _value;
+        public string format { get; private set; }
+
+        public string value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        public TimestampData(string format = "")
+        {
+            this.format = string.IsNullOrEmpty(format) ? DATE_TIME_FORMAT : format;
+            SetDateTime(UtcNow);
+        }
+
+        public void SetDateTime(System.DateTime dateTime) => _value = dateTime.ToString(format);
+
+        public System.DateTime GetDateTime() => string.IsNullOrEmpty(_value)
+            ? UtcNow
+            : ParseExact(_value, format, null);
+    }
+}
