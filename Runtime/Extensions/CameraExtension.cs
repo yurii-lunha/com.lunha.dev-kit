24adb40fca0e3cd5d6a5a436905c1f8dbff9597c
Yurii Lunha <yurii.lunha.dev@gmail.com>
2024-02-22 20:10:27 -0700

    refactor: camera extensions

--- Runtime/Extensions/CameraExtension.cs
+++ Runtime/Extensions/CameraExtension.cs
@@ -8,42 +8,105 @@ namespace Lunha.DevKit.Extensions
     public static class CameraExtension
     {
         /// <summary>
-        ///     Transforms a Input.mousePosition point from screen space into world space.
+        /// Transforms the mouse position from screen space to world space.
         /// </summary>
         /// <param name="camera"></param>
         /// <returns></returns>
         public static Vector2 TouchToWorldPoint(this Camera camera)
         {
-            Vector2 touchPoint = Input.mousePosition;
-
-            return camera.ScreenToWorldPoint(new Vector3(touchPoint.x, touchPoint.y, camera.nearClipPlane));
+            var touchPoint = Input.mousePosition;
+            var worldPoint = TouchToWorldPoint(camera, touchPoint);
+            return worldPoint;
         }
 
-        public static Vector2 TouchToWorldPoint(this Camera camera, Vector2 touchPoint) =>
-            camera.ScreenToWorldPoint(new Vector3(touchPoint.x, touchPoint.y, camera.nearClipPlane));
+        /// <summary>
+        /// Transforms the touch position from screen space to world space
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="touchPoint">Screen space position</param>
+        /// <returns></returns>
+        public static Vector2 TouchToWorldPoint(this Camera camera, Vector2 touchPoint)
+        {
+            var worldPoint = camera.ScreenToWorldPoint(new Vector3(touchPoint.x, touchPoint.y, camera.nearClipPlane));
+            return worldPoint;
+        }
 
-        public static RaycastHit2D TouchToRaycastHit(this Camera camera, string tag = "") =>
-            TouchToRaycastHit(camera, Input.mousePosition, tag);
+        /// <summary>
+        /// Cast a ray using the mouse position and get the first object
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="tag">Filter by tag</param>
+        /// <returns></returns>
+        public static RaycastHit2D TouchToRaycastHit(this Camera camera, string tag = "")
+        {
+            var hit = TouchToRaycastHit(camera, Input.mousePosition, tag);
+            return hit;
+        }
 
-        public static RaycastHit2D[] TouchToRaycastHits(this Camera camera, string tag = "") =>
-            TouchToRaycastHits(camera, Input.mousePosition, tag);
+        /// <summary>
+        /// Cast a ray using the mouse position
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="tag">Filter by tag</param>
+        /// <returns></returns>
+        public static RaycastHit2D[] TouchToRaycastHits(this Camera camera, string tag = "")
+        {
+            var hits = TouchToRaycastHits(camera, Input.mousePosition, tag);
+            return hits;
+        }
 
+        /// <summary>
+        /// Cast a ray using the screen position and get the first object
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="touchPosition">Screen space position</param>
+        /// <param name="tag">Filter by tag</param>
+        /// <returns></returns>
         public static RaycastHit2D TouchToRaycastHit(this Camera camera, Vector3 touchPosition, string tag = "")
         {
             var hits = TouchToRaycastHits(camera, touchPosition, tag);
-
-            return tag != string.Empty ? hits.FirstOrDefault(i => i.transform.CompareTag(tag)) : hits[0];
+            var hit = hits.FirstOrDefault();
+            return hit;
         }
 
+        /// <summary>
+        /// Cast a ray using the screen position
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="touchPosition">Screen space position</param>
+        /// <param name="tag">Filter by tag</param>
+        /// <returns></returns>
         public static RaycastHit2D[] TouchToRaycastHits(this Camera camera, Vector3 touchPosition, string tag = "")
         {
             var ray = camera.ScreenPointToRay(touchPosition);
             var hits = Physics2D.RaycastAll(ray.origin, ray.direction);
 
-            return tag != string.Empty ? hits.Where(i => i.transform.CompareTag(tag)).ToArray() : hits;
+            if (tag == string.Empty)
+            {
+                return hits;
+            }
+
+            var tagHits = hits
+                .Where(i =>
+                    i.collider.CompareTag(tag) ||
+                    i.transform.CompareTag(tag)
+                )
+                .ToArray();
+
+            return tagHits;
         }
 
-        public static RaycastHit2D WorldToRaycastHit(this Camera camera, Vector2 worldPosition, string tag = "") =>
-            TouchToRaycastHit(camera, camera.WorldToScreenPoint(worldPosition), tag);
+        /// <summary>
+        /// Cast a ray using the world position
+        /// </summary>
+        /// <param name="camera"></param>
+        /// <param name="worldPosition">Position in world coordinates</param>
+        /// <param name="tag">Filter by tag</param>
+        /// <returns></returns>
+        public static RaycastHit2D WorldToRaycastHit(this Camera camera, Vector2 worldPosition, string tag = "")
+        {
+            var hit = TouchToRaycastHit(camera, camera.WorldToScreenPoint(worldPosition), tag);
+            return hit;
+        }
     }
 }
